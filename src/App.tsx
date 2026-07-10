d4abc9778083fc1d64714f20c8176316e609bbeb
Oscar Graham <oscargraham06@gmail.com>
2026-07-10 14:11:09 -0500

    recommitting proper project layout

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div>
+      <h1>Garage Log</h1>
+
+      <h2>Add Service Entry</h2>
+
+      <form>
+        <input placeholder="Vehicle Name" />
+        <input placeholder="Mileage" />
+        <input placeholder="Service Performed" />
+        <button>Add Entry</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
